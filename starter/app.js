c7bcd05d4f38907d77be148d530f5662211a26fe
greenvald <greenvald@gmail>
2020-06-04 19:24:50 +0300

    first update

--- starter/app.js
+++ starter/app.js
@@ -9,13 +9,54 @@ GAME RULES:
 
 */
 
-let gamee = score,roundScore,activatePlayer,dice;
-score =[0,0];
-roundScore =0;
-activatePlayer=0;
-dice = rend(6);
-console.log(dice);
+let score, roundScore, activePlayer;
+score = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+// console.log(dice);
+// document.querySelector('#current-1').textContent=dice;
 
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function () {
+    let dice = Math.floor(Math.random() * 6) + 1;
+
+    let diceDom = document.querySelector('.dice');
+    // document.querySelector('#current-1').textContent=dice;
+
+    diceDom.style.display = 'block';
+    diceDom.src = 'dice-' + dice + '.png';
+    if (dice !== 1) {
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+
+    }
+    else {
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        document.querySelector('.player-0-panel ').classList.toggle('active');
+        document.querySelector('.player-1-panel ').classList.toggle('active');
+
+        document.querySelector('.dice').style.display = 'none';
+
+
+
+        // document.querySelector('.player-0-panel ').classList.remove('active');
+        // document.querySelector('.player-1-panel ').classList.add('active');
+
+
+    }
+
+
+
+
+});
 
 
 
